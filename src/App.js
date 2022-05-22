e43f663a46a3cf3f82d39e29219d876051f6a8b1
ZakirovEr <erhan.zakirov96@gmail.com>
2022-05-23 01:05:09 +0600

    first com

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Comment from './components/Comment'
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: "http://placekitten.com/g/64/64"
+  }
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Comment date={comment.date} text={comment.text} author={comment.author}/>
+    </div>
+  );
+}
+
+export default App;
